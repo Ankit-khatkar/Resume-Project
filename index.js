bb0c7d6ceeb1eadf64d7aea8494122acb715b900
Ankit Khatkar <Ankitofficial1504@gmail.com>
2022-05-03 10:58:04 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// Swiper Js Code.
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    autoplay:{
+        delay:2500
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
